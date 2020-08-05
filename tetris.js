990ed2282315aec7d57c0acd595fd72cc785d43b
imxande <alerodriguezlarosa@gmail.com>
2020-08-05 00:15:53 -0400

    Created 200 divs

--- tetris.js
+++ tetris.js
@@ -1,1 +1,19 @@
-console.log("Connected");
+document.addEventListener("DOMContentLoaded", () => {
+  // total number of divs
+  const count = 200;
+
+  // genereates 200 divs
+  for (let i = 0; i < count; i++) {
+    // create div element
+    const div = document.createElement("div");
+
+    // sanity check
+    div.innerHTML = "Hello, I am a div!";
+
+    // grab the grid id
+    const grid = document.getElementById("grid");
+
+    // append child to grid
+    grid.appendChild(div);
+  }
+});
