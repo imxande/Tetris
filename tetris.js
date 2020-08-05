5887186641106e3dcf66284a658ba6524e3d2906
imxande <alerodriguezlarosa@gmail.com>
2020-08-05 16:49:05 -0400

    styled grid and divs

--- tetris.js
+++ tetris.js
@@ -2,18 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
   // total number of divs
   const count = 200;
 
+  // grab the grid id
+  const grid = document.getElementById("grid");
+
+  // width
+  const width = 10;
+
   // genereates 200 divs
   for (let i = 0; i < count; i++) {
     // create div element
     const div = document.createElement("div");
 
-    // sanity check
-    div.innerHTML = "Hello, I am a div!";
-
-    // grab the grid id
-    const grid = document.getElementById("grid");
-
     // append child to grid
     grid.appendChild(div);
   }
+
+  // grabbing div and storing it in square variable
+  const square = document.querySelectorAll("#grid div");
+  console.log(square);
 });
